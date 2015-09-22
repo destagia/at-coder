a614deb0081f405f08400fa8c3ed1eb0094b8604
miyatin <kei@kamasu.jp>
2015-09-22 17:11:53 +0900

    fix

--- abc028/d.scala
+++ abc028/d.scala
@@ -1,13 +1,9 @@
 object Main {
   def main(args: Array[String]) {
-    val Array(n, k) = scala.io.StdIn.readLine().split(" ").map(_.toInt)
-    val all = getAll(n)
-    val low = k - 1
-    val up  = n - k
-    val n3 = low + up
-    val n6 = low * up
-    println((1 + n3 * 3 + n6 * 6).toDouble / all)
+    val Array(n, k) = scala.io.StdIn.readLine().split(" ").map(_.toDouble)
+    val all = n * n * n
+    val n3: Double = n - 1
+    val n6: Double = (k - 1) * (n - k)
+    println("%.15f".format((1 + n3 * 3 + n6 * 6).toDouble / all))
   }
-
-  def getAll(n: Int): Double = (n * n * n).toDouble
 }
