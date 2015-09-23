56c5831a50af7be7fc3caf8213f13575f7b376fe
miyatin <kei@kamasu.jp>
2015-09-24 04:15:17 +0900

    027 - c

--- abc027/c.scala
+++ abc027/c.scala
@@ -1,5 +1,43 @@
 object Main {
+
+  sealed trait Arrow { def reverse: Arrow }
+  case object Left extends Arrow { val reverse = Right }
+  case object Right extends Arrow { val reverse = Left }
+
+  sealed trait Turn { def arrow: Arrow }
+  case class Takahashi(arrow: Arrow) extends Turn
+  case class Aoki(arrow: Arrow) extends Turn
+
   def main(args: Array[String]) {
-    val input = scala.io.StdIn.readLine()
+    try {
+      val N = scala.io.StdIn.readLine().toLong
+      var i = 0
+      while(1L << (i + 1) <= N) { i += 1 }
+      val (tArrow, aArrow) = if (i % 2 == 0) (Right, Left) else (Left, Right)
+      val takahashi = Takahashi(tArrow)
+      simulate(N, takahashi) match {
+        case Takahashi(_) => println("Aoki")
+        case Aoki(_) => println("Takahashi")
+      }
+    }
+    catch {
+      case e: NumberFormatException => println("Aoki")
+    }
   }
+
+  def next(arrow: Arrow, num: Long): Long = arrow match {
+    case Left => num * 2
+    case Right => num * 2 + 1
+  }
+
+  def simulate(N: Long, turn: Turn, x: Long = 1): Turn = {
+    val y = next(turn.arrow, x)
+    if (y > N)
+      turn
+    else turn match {
+      case Aoki(arrow) => simulate(N, Takahashi(arrow.reverse), y)
+      case Takahashi(arrow) => simulate(N, Aoki(arrow.reverse), y)
+    }
+  }
+
 }
