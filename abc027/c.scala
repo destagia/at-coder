841e5dc1069142c35dd6de9e704c82f78f0ef9a9
miyatin <kei@kamasu.jp>
2015-09-24 12:43:32 +0900

    fix

--- abc027/c.scala
+++ abc027/c.scala
@@ -14,15 +14,12 @@ object Main {
       var i = 0
       while(1L << (i + 1) <= N) { i += 1 }
       val (tArrow, aArrow) = if (i % 2 == 0) (Right, Left) else (Left, Right)
-      val takahashi = Takahashi(tArrow)
-      simulate(N, takahashi) match {
+      simulate(N, Takahashi(tArrow), Aoki(aArrow)) match {
         case Takahashi(_) => println("Aoki")
         case Aoki(_) => println("Takahashi")
       }
     }
-    catch {
-      case e: NumberFormatException => println("Aoki")
-    }
+    catch { case e: NumberFormatException => println("Aoki") }
   }
 
   def next(arrow: Arrow, num: Long): Long = arrow match {
@@ -30,14 +27,10 @@ object Main {
     case Right => num * 2 + 1
   }
 
-  def simulate(N: Long, turn: Turn, x: Long = 1): Turn = {
+  def simulate(N: Long, turn: Turn, nextTurn: Turn, x: Long = 1): Turn = {
     val y = next(turn.arrow, x)
-    if (y > N)
-      turn
-    else turn match {
-      case Aoki(arrow) => simulate(N, Takahashi(arrow.reverse), y)
-      case Takahashi(arrow) => simulate(N, Aoki(arrow.reverse), y)
-    }
+    if (y > N) turn
+    else       simulate(N, nextTurn, turn, y)
   }
 
 }
